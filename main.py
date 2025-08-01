e36f0bef7d75b5a73b4041f3fa2703897eae4959
Arthur Budny <budnyarthur@gmail.com>
2025-08-01 18:20:14 -0300

    feat: show movie info with bootstrap and hides api key

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 import requests
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 app = Flask(__name__)
 details_url = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjNmNTRlZTllNmQ5ZWU0MzRlMzA2YjhjNTBiNTU1ZiIsIm5iZiI6MTc0OTU2NTEzMi44MTYsInN1YiI6IjY4NDgzZWNjNjZlMTZhNWEzOTIwNjkzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BxpAUpU5jMx19rLcqHrpH5r7FV6xhTP-7F60HkHw82E"
+    "Authorization": api_key
 }
 
 @app.route("/", methods=["POST", "GET"])
@@ -22,8 +26,10 @@ def home():
 def moviepage(title):
     response = requests.get(f"https://api.themoviedb.org/3/search/movie?query={title}&include_adult=false&language=en-US&page=1", headers=headers)
     movie_info = response.json()
-    return f"{movie_info["results"][0]}"
-
+    try:
+        return render_template("moviepage.html", info = movie_info["results"])
+    except IndexError:
+        return f"No results :("
 
 if __name__ == "__main__":
     app.run(debug=True)
