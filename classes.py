1cb240a6132b498a679332863c44beb50f8aa47b
Arthur Budny <budnyarthur@gmail.com>
2025-08-15 16:11:23 -0300

    feat: add user favorite
    Create MovieCache model to store movie information without calling API again.
    Implement intermediate table to create N:N relationship between users and movies

--- classes.py
+++ classes.py
@@ -1,12 +1,36 @@
 from config import db, UserMixin
 
+favorites = db.Table("favorites",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("tmdb_id", db.Integer, db.ForeignKey("moviecache.tmdb_id"))
+)   
+
 class User(db.Model, UserMixin):
     id = db.Column("id",db.Integer, primary_key=True)
     name = db.Column("name",db.String(20), unique=True, nullable=False)
     email = db.Column("email",db.String(100), unique=True, nullable=False)
     password = db.Column("password", db.String(100), nullable=False)
+    fav_movies = db.relationship("MovieCache", secondary=favorites, backref="fav_for_user")
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+
+class MovieCache(db.Model):
+    __tablename__ = "moviecache"
+
+    id = db.Column("id", db.Integer, primary_key=True)
+    tmdb_id = db.Column("tmdb_id",db.Integer)
+    title = db.Column("title", db.String(50))
+    poster_path = db.Column("poster_path", db.String(80))
+    release_date = db.Column("release_date", db.String(15))
+
+    def __init__(self, tmdb_id, title, poster_path, release_date):
+        self.tmdb_id = tmdb_id
+        self.title = title
+        self.poster_path = poster_path
+        self.release_date = release_date
+
+    def __repr__(self):
+        return f"{self.tmdb_id}, {self.title}, {self.poster_path}, {self.release_date}"
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from config import db, request, redirect, url_for, render_template, requests, lm, app, bcrypt, api_key, login_user, login_required, flash, dummy_hash, current_user, logout_user, languages_url, headers
-from classes import User
-
-@lm.user_loader
-def user_loader(id):
-    user = db.session.query(User).filter_by(id=id).first()
-    return user
+from classes import User, MovieCache
 
 @lm.user_loader
 def user_loader(id):
@@ -43,7 +38,6 @@ def register():
     
     else:
         return render_template("register.html")
-
 
 @app.route("/login", methods= ["POST", "GET"])
 def login():
@@ -87,92 +81,9 @@ def user(user):
 
     if user_found:
         if user_found == current_user:
-            return render_template("user.html", user=user_found, admin=True)
+            return render_template("user.html", user=user_found, admin=True, favorites=current_user.fav_movies)
         else:
-            return render_template("user.html", user=user_found, admin=False)
-    else:
-        return f"Something went wrong! Try again later"
-
-
-@app.route("/view")
-def view():
-    return render_template("view.html", values = User.query.all())
-
-@app.route("/logout")
-@login_required
-def logout():
-    flash("Logged out!")
-    logout_user()
-    return redirect("/")
-
-    
-@app.route("/register", methods=["POST","GET"])
-def register():
-    if request.method == "POST":
-        name = request.form["nm"]
-        email = request.form["email"]
-
-        if User.query.filter_by(name=name, email=email).first() == None:    
-            password = request.form["password"]
-            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-
-            new_user = User(name, email, hashed_password)
-            db.session.add(new_user)
-            db.session.commit()
-
-            login_user(new_user)
-            return redirect(url_for("user",user=new_user.name))
-        
-        else:
-            flash("Username already in use")
-            return redirect(url_for("register"))
-    
-    else:
-        return render_template("register.html")
-
-
-@app.route("/login", methods= ["POST", "GET"])
-def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-
-        user = User.query.filter_by(email=email).first()
-        if user:        
-            if bcrypt.check_password_hash(user.password, password):
-                login_user(user)
-                return redirect(url_for("user", user=user.name))
-        else:
-            bcrypt.check_password_hash(dummy_hash, password)
-            flash("Invalid credentials.")
-            return redirect(url_for("login"))
-    else:
-        return render_template("login.html")
-
-@app.route("/profile")
-@login_required
-def profile():
-    return redirect(url_for("user", user=current_user.name))
-
-@app.route("/search")
-def search():
-    search_user_name = request.args["search_user"]
-    search_user = User.query.filter_by(name=search_user_name).first()
-    if search_user:
-        return redirect(url_for("user", user=search_user.name))
-    else:
-        flash("User not found! :(")
-        return redirect(url_for("home"))
-
-@app.route("/<user>")
-def user(user):
-    user_found = User.query.filter_by(name=user).first()
-
-    if user_found:
-        if user_found == current_user:
-            return render_template("user.html", user=user_found, admin=True)
-        else:
-            return render_template("user.html", user=user_found, admin=False)
+            return render_template("user.html", user=user_found, admin=False, favorites=current_user.fav_movies)
     else:
         return f"Something went wrong! Try again later"
 
@@ -190,12 +101,34 @@ def logout():
 
 @app.route("/movie/<lang>/<title>")
 def moviepage(title, lang):
-    response = requests.get(f"https://api.themoviedb.org/3/search/movie?query={title}&include_adult=false&language={lang}&page=1", headers=headers)
-    movie_info = response.json()
-    try:
-        return render_template("moviepage.html", info = movie_info["results"])
-    except IndexError:
-        return f"No results :("
+        response = requests.get(f"https://api.themoviedb.org/3/search/movie?query={title}&include_adult=false&language={lang}&page=1", headers=headers)
+        movie_info = response.json()
+        results = movie_info["results"]
+
+        for movie in results:
+            search_movie = MovieCache.query.filter_by(tmdb_id=movie["id"]).first()
+            if search_movie == None:
+                movie_cache = MovieCache(movie["id"], movie["title"], movie["poster_path"], movie["release_date"])
+                db.session.add(movie_cache)
+                db.session.commit()
+
+        try:
+            return render_template("moviepage.html", info=results, logged_in=current_user, title=title, lang=lang)
+        except IndexError:
+            return f"No results :("
+
+@app.route("/movie/favorite", methods=["POST"])
+def favorite():
+    fav_id = request.form["fav_id"]
+    title = request.form["title"]
+    lang = request.form["lang"]
+    search_movie = MovieCache.query.filter_by(tmdb_id=int(fav_id)).first()
+
+    if search_movie not in current_user.fav_movies:
+        current_user.fav_movies.append(search_movie)
+        db.session.commit()
+    
+    return redirect(url_for("moviepage", title=title, lang=lang))
 
 if __name__ == "__main__":
     with app.app_context():
